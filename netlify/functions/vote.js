5e0b4b3a505a1eab516a3a5b4e08da0ab4ae2916
u7mi <115493111+u7mi@users.noreply.github.com>
2025-12-30 08:50:54 +0300

    Update vote.js

--- netlify/functions/vote.js
+++ netlify/functions/vote.js
@@ -1,6 +1,24 @@
+import { createClient } from '@supabase/supabase-js'
+
 export const handler = async () => {
+  const supabase = createClient(
+    process.env.SUPABASE_URL,
+    process.env.SUPABASE_SERVICE_KEY
+  )
+
+  const { data, error } = await supabase
+    .from('votes')
+    .insert([{ voter_id: 'TEST123', barber_id: 1, barber_name: 'TEST' }])
+
+  if (error) {
+    return {
+      statusCode: 500,
+      body: JSON.stringify(error)
+    }
+  }
+
   return {
     statusCode: 200,
-    body: 'FUNCTION_WORKS'
+    body: 'INSERT_OK'
   }
 }
